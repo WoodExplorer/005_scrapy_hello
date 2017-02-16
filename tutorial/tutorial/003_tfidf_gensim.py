332f5572008eacf7d5766c6eec1c696b46a4dbaf
WoodExplorer <593916342@qq.com>
2017-02-16 10:29:28 +0800

    [Linux] Try out tfidf

--- tutorial/tutorial/003_tfidf_gensim.py
+++ tutorial/tutorial/003_tfidf_gensim.py
@@ -3,49 +3,14 @@
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-from gensim import corpora
-
-
-documents = ["Human machine interface for lab abc computer applications",
-             "A survey of user opinion of computer system response time",
-             "The EPS user interface management system",
-             "System and human system engineering testing of EPS",
-             "Relation of user perceived response time to error measurement",
-             "The generation of random binary unordered trees",
-             "The intersection graph of paths in trees",
-             "Graph minors IV Widths of trees and well quasi ordering",
-             "Graph minors A survey"]
+from gensim import corpora, models, similarities
 
 
 # remove common words and tokenize
 stoplist = set('for a of the and to in'.split())
-texts = [[word for word in document.lower().split() if word not in stoplist]
-         for document in documents]
-
-# remove words that appear only once
-from collections import defaultdict
-frequency = defaultdict(int)
-for text in texts:
-    for token in text:
-        frequency[token] += 1
-
-texts = [[token for token in text if frequency[token] > 1]
-         for text in texts]
-
-from pprint import pprint  # pretty-printer
-pprint(texts)
-#[['human', 'interface', 'computer'],
-# ['survey', 'user', 'computer', 'system', 'response', 'time'],
-# ['eps', 'user', 'interface', 'system'],
-# ['system', 'human', 'system', 'eps'],
-# ['user', 'response', 'time'],
-# ['trees'],
-# ['graph', 'trees'],
-# ['graph', 'minors', 'trees'],
-# ['graph', 'minors', 'survey']]
 
 
-# memory-efficient dictionary construction
+# memory-efficient dictionary construction <START>
 from six import iteritems
 # collect statistics about all tokens
 dictionary = corpora.Dictionary(line.lower().split() for line in open('mycorpus.txt'))
@@ -58,8 +23,9 @@ dictionary.filter_tokens(stop_ids + once_ids)  # remove stop words and words tha
 dictionary.save('deerwester.dict')  # store the dictionary, for future reference
 print(dictionary)
 print(dictionary.token2id)
-# END
+# <END>
 
+# memory-efficient corpus construction <START>
 class MyCorpus(object):
        def __iter__(self):
            for line in open('mycorpus.txt'):
@@ -68,20 +34,44 @@ class MyCorpus(object):
 
 corpus_memory_friendly = MyCorpus()  # doesn't load the corpus into memory!
 print(corpus_memory_friendly)
+corpora.MmCorpus.serialize('/tmp/deerwester.mm', corpus_memory_friendly)  # store to disk, for later use
 
 for vector in corpus_memory_friendly:  # load one vector into memory at a time
      print(vector)
-
+# <END>
 
 # To actually convert tokenized documents to vectors:
 
-new_doc = "Human computer interaction"
+#new_doc = "Human computer interaction"
+new_doc = "time interface"
 new_vec = dictionary.doc2bow(new_doc.lower().split())
 print(new_vec)  # the word "interaction" does not appear in the dictionary and is ignored
 #[(0, 1), (1, 1)]
 
 
-corpus = [dictionary.doc2bow(text) for text in texts]
-corpora.MmCorpus.serialize('/tmp/deerwester.mm', corpus)  # store to disk, for later use
-print(corpus)
+
+# 
+tfidf = models.TfidfModel(corpus_memory_friendly)
+
+# To transform the whole corpus via TfIdf and index it, in preparation for similarity queries:
+index = similarities.SparseMatrixSimilarity(tfidf[corpus_memory_friendly], num_features=12)
+
+
+sims = index[tfidf[new_vec]]
+print(list(enumerate(sims)))
+
+
+#>>> a1 = 0.7071067811865476
+#>>> a0 = 0.7071067811865476
+#>>> b0, b1, b2 = 0.5773502691896257,  0.5773502691896257, 0.5773502691896257
+#
+#print("tfidf representation of new_vec")
+#print(tfidf[new_vec])
+#print("tfidf representation of corpus")
+#for vector in corpus_memory_friendly:  # load one vector into memory at a time
+#     print(tfidf[vector])
+#
+#>>> import math
+#>>> a1 * b2 / (math.sqrt(a0 ** 2 + a1 **2) * math.sqrt(b0 ** 2 + b1 ** 2 + b2 ** 2))
+#0.408248290463863
 
